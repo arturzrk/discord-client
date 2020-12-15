2ea5211719f3d527bd2dd4c052b7cbbc54dccc4d
Artur Zurek <artur.zurek@ssginsight.com>
2020-12-15 10:57:52 +0100

    About page + added bootstrap.

---
+++ src/components/Component.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ message }}
+    <div>{{ message2 }}</div>
+    <button class="btn btn-primary" type="button">Bootstrap button test</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Prop, Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class ArturComponent extends Vue {
+  @Prop() private message!: string;
+
+  message2 = 'This is declared in the class';
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1><ArturAbout message="With Message Passed as a Property"/></h1>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import ArturAbout from '@/components/Component.vue';
+
+@Component({
+  components: {
+    ArturAbout,
+  },
+})
+export default class About extends Vue {}
+</script>
